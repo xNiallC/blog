25f0b75d6ffea2aeacc279647615d0b49da7ef55
Niall Curtis <niallycurtis@gmail.com>
2020-07-25 12:18:22 +0100

    Added deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@ const { apiEndpoint } = require('./prismic-config');
 var repo = /([^\/]+)\.prismic\.io\/graphql/.exec(apiEndpoint);
 
 module.exports = {
+  pathPrefix: '/blog',
   siteMetadata: {
     title: `Gatsby Prismic Blog`,
     description: `Blog example for Gatsby & Prismic`,
